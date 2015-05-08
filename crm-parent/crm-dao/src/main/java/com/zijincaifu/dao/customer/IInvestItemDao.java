76e3f1255829ab9910a123aa7a83d1e42167e507
djx <djx@sxj.com>
2015-05-08 15:31:58 +0800

    Change-Id: Ic9cbddd12ebf66c50bbcf0d9af79899faa50b82d

---
+++ crm-parent/crm-dao/src/main/java/com/zijincaifu/dao/customer/IInvestItemDao.java
@@ -0,0 +1,23 @@
+package com.zijincaifu.dao.customer;
+
+import java.util.List;
+
+import com.sxj.mybatis.orm.annotations.Insert;
+import com.sxj.mybatis.orm.annotations.Update;
+import com.sxj.util.persistent.QueryCondition;
+import com.zijincaifu.entity.customer.InvestItemEntity;
+
+public interface IInvestItemDao
+{
+    @Insert
+    public void addItem(InvestItemEntity item);
+    
+    @Update
+    public void updateItem(InvestItemEntity item);
+    
+    public void deleteItems(String customerId);
+    
+    public List<InvestItemEntity> queryItemList(
+            QueryCondition<InvestItemEntity> query);
+    
+}
---
+++ crm-parent/crm-dao/src/main/java/com/zijincaifu/dao/customer/ITrackRecordDao.java
@@ -0,0 +1,20 @@
+package com.zijincaifu.dao.customer;
+
+import java.util.List;
+
+import com.sxj.mybatis.orm.annotations.Insert;
+import com.sxj.util.persistent.QueryCondition;
+import com.zijincaifu.entity.customer.TrackRecordEntity;
+
+public interface ITrackRecordDao
+{
+    
+    @Insert
+    public void addTrackRecord(TrackRecordEntity record);
+    
+    public void deleteTrackRecord(String customerId);
+    
+    public List<TrackRecordEntity> queryTrackRecordList(
+            QueryCondition<TrackRecordEntity> query);
+    
+}
