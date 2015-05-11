e9783bfe57f7aca0d0426457d2d1dc6bcc274bd6
djx <djx@sxj.com>
2015-05-11 18:53:59 +0800

    message

--- crm-parent/crm-dao/src/main/java/com/zijincaifu/crm/entity/personnel/PersonnelEntity.java
+++ crm-parent/crm-dao/src/main/java/com/zijincaifu/crm/entity/personnel/PersonnelEntity.java
@@ -20,25 +20,6 @@ import com.zijincaifu.crm.enu.personnel.PersonnelCompanyEnum;
  *
  */
 @Entity(mapper = IPersonnelDao.class)
-@Table(name="CRM_PERSONNEL")
-public class PersonnelEntity extends Pagable implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6604367198110501481L;
-	
-	/**
-	 * 主键
-	 */
-	@Id(column="ID")
-	@GeneratedValue(strategy = GenerationType.UUID)
-	private String id;
-	
-	/**
-	 * 员工编号
-	 */
-	@Column(name="UID")
 @Table(name = "CRM_PERSONNEL")
 public class PersonnelEntity extends Pagable implements Serializable
 {
@@ -78,7 +59,7 @@ public class PersonnelEntity extends Pagable implements Serializable
      * 所属分公司
      */
     @Column(name = "COMPANY")
-    private Integer company;
+    private PersonnelCompanyEnum company;
     
     /**
      * 关注微信后生成的UnionId
@@ -135,30 +116,6 @@ public class PersonnelEntity extends Pagable implements Serializable
     }
     
     public String getPhone()
-@Table(name="CRM_PERSONNEL")
-public class PersonnelEntity extends Pagable implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6604367198110501481L;
-	
-	/**
-	 * 主键
-	 */
-	@Id(column="ID")
-	@GeneratedValue(strategy = GenerationType.UUID)
-	private String id;
-	
-	/**
-	 * 员工编号
-	 */
-	@Column(name="UID")
-	 @Sn(pattern = "00000", step = 1, table = "T_SN", stubValue = "E", stub = "F_SN_NAME", sn = "F_SN_NUMBER")
-	private PersonnelCompanyEnum company;
-	
-
-	public PersonnelCompanyEnum getCompany()
     {
         return phone;
     }
@@ -168,14 +125,54 @@ public class PersonnelEntity extends Pagable implements Serializable {
         this.phone = phone;
     }
     
-    public Integer getCompany()
+    public PersonnelCompanyEnum getCompany()
     {
-
-	public void setCompany(Integer company) {
-		this.company = company;
-	}
+        return company;
+    }
     
-    public void setCompany(Integer company)
-
     public void setCompany(PersonnelCompanyEnum company)
+    {
+        this.company = company;
+    }
+    
+    public String getUnionId()
+    {
+        return unionId;
+    }
+    
+    public void setUnionId(String unionId)
+    {
+        this.unionId = unionId;
+    }
+    
+    public Date getAddTime()
+    {
+        return addTime;
+    }
+    
+    public void setAddTime(Date addTime)
+    {
+        this.addTime = addTime;
+    }
+    
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    
+    public Integer getFreezeStatus()
+    {
+        return freezeStatus;
+    }
+    
+    public void setFreezeStatus(Integer freezeStatus)
+    {
+        this.freezeStatus = freezeStatus;
+    }
+    
 }
