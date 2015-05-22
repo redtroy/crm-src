1fb12e35cff3c2d9470e52eee587711237c51294
djx <djx@sxj.com>
2015-05-22 14:53:21 +0800

    Change-Id: I2dac4feb879086385c0d88ddf48709108c20e5d3

--- crm-parent/crm-manager/src/main/java/com/zijincaifu/crm/manage/controller/weixin/OpenWeixinController.java
+++ crm-parent/crm-manager/src/main/java/com/zijincaifu/crm/manage/controller/weixin/OpenWeixinController.java
@@ -1,5 +1,6 @@
 package com.zijincaifu.crm.manage.controller.weixin;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.sxj.spring.modules.mapper.JsonMapper;
 import com.sxj.spring.modules.web.MediaTypes;
 import com.sxj.util.common.StringUtils;
+import com.sxj.util.logger.SxjLogger;
 import com.zijincaifu.crm.manage.controller.BaseController;
 import com.zijincaifu.model.customer.OpenCustomerModel;
 import com.zijincaifu.service.customer.ICustomerService;
@@ -28,30 +30,33 @@ public class OpenWeixinController extends BaseController
     @RequestMapping(value = "/addCustomer", method = RequestMethod.POST, consumes = MediaTypes.JSON)
     @ResponseBody
     public void addCustomer(@RequestBody String json,
-            HttpServletResponse response)
+            HttpServletResponse response) throws IOException
     {
+        PrintWriter out = response.getWriter();
         try
         {
-            PrintWriter out = response.getWriter();
+            
             if (StringUtils.isEmpty(json))
             {
-                out.print("1");
+                out.print("0");
                 return;
             }
-            
             OpenCustomerModel model = JsonMapper.nonEmptyMapper()
                     .getMapper()
                     .readValue(json, OpenCustomerModel.class);
-            
+            customerService.addWeixinCustomer(model);
+            out.print("1");
+            return;
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            SxjLogger.error(e.getMessage(), e, this.getClass());
+            out.print("0");
         }
         finally
         {
-            //out.flush();
-            //out.close();
+            out.flush();
+            out.close();
         }
         
     }
