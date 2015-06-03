93cbc96ab194e1326ceabcacc529996ceabfe215
djx <djx@sxj.com>
2015-06-03 11:27:12 +0800

    Change-Id: I642fe179bb7eeac065cd270d6ec1abb3a7db26bf

--- crm-parent/crm-dao/src/main/java/com/zijincaifu/crm/model/channel/ChannelModel.java
+++ crm-parent/crm-dao/src/main/java/com/zijincaifu/crm/model/channel/ChannelModel.java
@@ -9,88 +9,94 @@ import com.zijincaifu.crm.entity.personnel.PersonnelEntity;
 
 public class ChannelModel extends Pagable implements Serializable
 {
-
+    
     /**
      * 
      */
     private static final long serialVersionUID = -7683567106040725618L;
     
-    private ChannelEntity channel=new ChannelEntity();
+    private ChannelEntity channel = new ChannelEntity();
     
-    private PersonnelEntity personnel=new PersonnelEntity();
-
+    private PersonnelEntity personnel = new PersonnelEntity();
+    
     public String getId()
     {
         return channel.getId();
     }
-
+    
     public void setId(String id)
     {
         channel.setId(id);
     }
-
+    
     public String getChannelId()
     {
         return channel.getChannelId();
     }
-
+    
     public void setChannelId(String channelId)
     {
         channel.setChannelId(channelId);
     }
-
+    
     public String getName()
     {
         return channel.getName();
     }
-
+    
     public void setName(String name)
     {
         channel.setName(name);
     }
-
+    
     public String getRemark()
     {
         return channel.getRemark();
     }
-
+    
     public void setRemark(String remark)
     {
         channel.setRemark(remark);
     }
-
+    
     public Date getStartTime()
     {
         return channel.getStartTime();
     }
-
+    
     public void setStartTime(Date startTime)
     {
         channel.setStartTime(startTime);
     }
-
+    
     public String getUid()
     {
         return channel.getUid();
     }
-
+    
     public void setUid(String uid)
     {
         channel.setUid(uid);
     }
-
+    
     public String getPersonnelName()
     {
         return personnel.getName();
     }
-
+    
     public void setPersonnelName(String name)
     {
         personnel.setName(name);
     }
     
+    public Boolean getIsBind()
+    {
+        return channel.getIsBind();
+    }
     
-    
-    
+    public void setIsBind(Boolean isBind)
+    {
+        channel.setIsBind(isBind);
+    }
     
 }
